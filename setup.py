46237897e11298149b63c5cbdfeadf893f38fa87
Sascha Sauermann <sascha.sauermann@tum.de>
2018-06-22 19:33:26 +0200

    Added setup.py and travis and codacy config files.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='job-combine',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/ssauermann/job-combine',
+    license='MIT',
+    author='Sascha Sauermann',
+    author_email='saschasauermann@gmx.de',
+    description='Combine similar job scripts for workload managers like SLURM or Loadleveler into fewer scripts '
+                'executing the original jobs sequentially to prevent wasting resources when running multiple short '
+                'scripts.',
+    entry_points={
+        'console_scripts': [
+            'job-combine = job_combine.job_combine:main',
+        ],
+    },
+)
