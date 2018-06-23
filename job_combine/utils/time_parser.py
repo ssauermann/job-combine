73672f9077b03ffcfde7a8ba3614ec0e94e827ff
Sascha Sauermann <sascha.sauermann@tum.de>
2018-06-23 17:43:54 +0200

    Fixed: Python 2.6 is missing timedelta.total_seconds method.

--- job_combine/utils/time_parser.py
+++ job_combine/utils/time_parser.py
@@ -8,6 +8,7 @@ Supported format specifiers are:
     %S for seconds
 """
 import re
+import sys
 from datetime import timedelta
 
 
@@ -56,21 +57,19 @@ def str_from_timedelta(time_delta, time_format):
     remaining_time = time_delta
     days = hours = minutes = 0
 
-    remaining_time.total_seconds()
-
     if '%D' in time_format:
         days = remaining_time.days
         remaining_time -= timedelta(days=days)
 
     if '%H' in time_format:
-        hours = int(remaining_time.total_seconds() / (60 * 60))
+        hours = int(total_seconds(remaining_time) / (60 * 60))
         remaining_time -= timedelta(hours=hours)
 
     if '%M' in time_format:
-        minutes = int(remaining_time.total_seconds() / 60)
+        minutes = int(total_seconds(remaining_time) / 60)
         remaining_time -= timedelta(minutes=minutes)
 
-    seconds = remaining_time.total_seconds()
+    seconds = total_seconds(remaining_time)
 
     formatted = time_format.replace('%D', '%02d' % days)
     formatted = formatted.replace('%H', '%02d' % hours)
@@ -78,3 +77,11 @@ def str_from_timedelta(time_delta, time_format):
     formatted = formatted.replace('%S', '%02d' % seconds)
 
     return formatted
+
+
+if sys.version_info >= (2, 7):
+    def total_seconds(td):
+        return td.total_seconds()
+else:
+    def total_seconds(td):
+        return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 1e6) / 1e6
